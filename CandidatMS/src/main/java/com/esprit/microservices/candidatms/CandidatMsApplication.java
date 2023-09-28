3b7141c2ce528556214dac3a043bfcb4dcf3ea29
Housseem-Kouki <houssem.kouki@esprit.tn>
2023-09-28 16:08:10 +0200

    preparation eureka service

--- CandidatMS/src/main/java/com/esprit/microservices/candidatms/CandidatMsApplication.java
+++ CandidatMS/src/main/java/com/esprit/microservices/candidatms/CandidatMsApplication.java
@@ -2,8 +2,10 @@ package com.esprit.microservices.candidatms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CandidatMsApplication {
 
     public static void main(String[] args) {
